fca66e3ee9d636e7ee6636a72b912a84cebc9ce7
Jeongmin Byun <jmbyun91@gmail.com>
2017-07-24 18:04:21 +0900

    upload static files retrieved from uilab web server

---
+++ WSDM11/index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+		<title>Users &amp; Information Lab</title> 
+		<link rel="shortcut icon" href="/images/uilab_fav.png" /> 
+		<script type="text/javascript">
+			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+			try {
+				var pageTracker = _gat._getTracker("UA-2589526-2");
+				pageTracker._trackPageview();
+			} catch(err) {}
+		</script>
+		<style>
+			div { margin: 0 0 50px 0; padding: 0; }
+			p { margin: 0; padding: 0; }
+			h2 { margin-bottom: 10px; }
+		</style>
+<body>
+	<div>
+		<h1>Aspect and Sentiment Unification Model for Online Review Analysis</h1>
+		<p>Yohan Jo (yohan.jo&#64;kaist.ac.kr)</p>
+		<p>Alice Oh (alice.oh&#64;kaist.edu)</p>
+		<p>Department of Computer Science, KAIST, Daejeon, Korea</p>
+
+		<br />
+		<p><a href="http://portal.acm.org/citation.cfm?id=1935932&CFID=8905137&CFTOKEN=-32037455">[URL]</a> <a href="citation.bib">[bibTeX]</a></p>
+	</div>
+
+	<div>
+		<h2>Data</h2>
+		<ul>
+			<li>Electronic device reviews (Amazon) <a href="Amazon.zip">[Link]</a></li>
+			<li>Restaurant reviews (Yelp) <a href="Yelp.zip">[Link]</a></li>
+		</ul>
+		<p>NOTE: Some reviews may appear multiple times</p>
+	</div>
+
+	<div>
+		<h2>Source Code</h2>
+		<p><a href="ASUM.zip">Java Code</a></p>
+	</div>
+
+	<div>
+		<p> If you have questions please send an email to <b>yohan.jo&#64;kaist.ac.kr</b></p>
+	</div>
+</body>
+</html>
